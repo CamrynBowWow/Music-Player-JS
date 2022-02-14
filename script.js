b98b05d7e6d8c49aed41ea669af8d042b1f45701
SmallPeenwow <bowdencamryn@gmail.com>
2022-02-14 15:42:17 +0200

    Side bar Two

--- script.js
+++ script.js
@@ -44,23 +44,38 @@ function toggleChange(){
 
 // Sidebar function
 
-const menuDisplay = document.querySelector('.menu-items');
+const menuDisplay = document.querySelector('#menu-items');
 const headerSize = document.querySelector('aside');
 
 function sidebar() {
 
-    if(menuDisplay.style.display === 'none'){
-        menuDisplay.style.display = 'grid';
+    if(menuDisplay.className === 'hidden' ){
         headerSize.style.width = '12rem';
         headerSize.style.height = '100vh';
-        headerSize.style.borderBottomRightRadius = '0px';
+        headerSize.style.borderBottomRightRadius = '0px';      
         headerSize.style.boxShadow = 'rgb(0 0 0 / 1) 1.95px 1.95px 2.6px';
+        // headerSize.querySelector('i.fas').classList.remove('fa-bars');
+        // headerSize.querySelector('i.fas').classList.add('fa-times');
+        menuDisplay.classList.remove('hidden');
+        menuDisplay.classList.add('show');
+
+        let myTime = setTimeout(() => {
+            menuDisplay.style.transform = 'translateX(0rem)';
+        }, 100);      
     } else {
-        menuDisplay.style.display = 'none';
-        headerSize.style.width = '4rem';
-        headerSize.style.height = '4rem';
-        headerSize.style.borderBottomRightRadius = '15px';
-        headerSize.style.boxShadow = 'rgb(0 0 0 / 51%) 1.95px 1.95px 2.6px';
+        menuDisplay.style.transform = 'translateX(-12rem)';
+        menuDisplay.classList.remove('show');
+        menuDisplay.classList.add('hidden');
+        
+        let myTimeRemove = setTimeout(() => {
+            headerSize.style.width = '4rem';
+            headerSize.style.height = '4rem';
+            headerSize.style.borderBottomRightRadius = '15px';
+            headerSize.style.boxShadow = 'rgb(0 0 0 / 51%) 1.95px 1.95px 2.6px';
+            // headerSize.querySelector('i.fas').classList.remove('fa-times');
+            // headerSize.querySelector('i.fas').classList.add('fa-bars');
+        }, 500);
+        
     }
 }
 
